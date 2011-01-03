c36f8d9b314bfae5a65220f11c78c32526c01430
Will Cannings <me@willcannings.com>
2011-01-03 16:40:07 +1100

    Auth required to add kills

--- app/controllers/kills_controller.rb
+++ app/controllers/kills_controller.rb
@@ -1,4 +1,13 @@
 class KillsController < ApplicationController
+  before_filter :password_required
+  
+  def password_required
+    return if Rails.env.development?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ncss' && password == 'ncss1102james_tara'
+    end
+  end
+  
   def index
     @kills = Kill.all
   end
