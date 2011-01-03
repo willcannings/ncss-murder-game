1f309f25e33748c9135211338a2510f44ebe5e9d
Will Cannings <me@willcannings.com>
2011-01-03 12:43:10 +1100

    Kill graph... sort of

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,5 +4,14 @@ class IndexController < ApplicationController
     @latest_kill = Kill.order('created_at desc').first
     @first_kill = Kill.order('created_at asc').first
     @serial_killer = Player.order('kills desc').first
+    
+    # create the graph kill data
+    kill_data = Kill.all.collect {|kill| "\"#{kill.killer.name}\" -> \"#{kill.killee.name}\";"}.join("\n")
+    
+    # draw the graph
+    pipe = IO.popen('/usr/bin/env dot -q -Tsvg', 'w+')
+    pipe.puts "digraph kills { #{kill_data} }"
+    pipe.close_write
+    @graph_data = pipe.read
   end
 end
