8234a2d8cc5af2e63a18fcfdf90da7fd5a509108
Will Cannings <me@willcannings.com>
2011-01-04 12:26:44 +1100

    Increment kills correctly

--- app/models/kill.rb
+++ app/models/kill.rb
@@ -3,6 +3,7 @@ class Kill < ActiveRecord::Base
   belongs_to :killee, class_name: 'Player'
   
   after_save do |kill|
-    kill.killer.increment(:kills)
+    kill.killer.kills = Kill.where(killer_id: kill.killer_id).count
+    kill.killer.save
   end
 end
